e8f277a5b158495fb560e39abbaa8a5b663c25cc
Daniel <stuermer.dan137@gmail.com>
2026-02-18 14:50:40 +0100

    deleted unneccessary file // included url data and functions

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 function init() {
     addSmallPokemonCard();
+    usePokeData();
 }
 
 function addSmallPokemonCard() {
--- scripts/remoteStorage.js
+++ scripts/remoteStorage.js
@@ -1,5 +1,13 @@
-URL_DATA = "https://pokeapi.co/api/v2/pokemon?limit=100&offset=151";
+BASE_URL = "https://pokeapi.co/api/v2/pokemon?limit=100&offset=151";
 
-function fetchPokeData() {
-    
+let pokeData;
+
+async function fetchPokeData(path="") {
+    let response = await fetch(BASE_URL + path + ".json");
+    let responseAsJson = await response.json();
+    return responseAsJson;
+}
+
+async function usePokeData() {
+    let pokeData = await fetchPokeData();
 }
