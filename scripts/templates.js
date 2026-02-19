4906927e12e77e2d08120f021c41a24a2ce36214
Daniel <stuermer.dan137@gmail.com>
2026-02-19 15:00:15 +0100

    added more color options and types // included them // style fixes

--- scripts/templates.js
+++ scripts/templates.js
@@ -1,12 +1,14 @@
 function smallPokemonCardTemplate(stats) {  // ich habe auf alle daten zugriff.
     let typesHTML = stats.types.map(typeObj => {  // ich erstelle ein HTML, dass eingefügt wird je nach anzahl der typen
         return `
-            <p class="small-card-types bg_${typeObj.type.name}">${typeObj.type.name}</p>
+            <p class="small-card-types type_${typeObj.type.name}">${typeObj.type.name}</p>
         `;
     }).join("");
+
+    let mainType = stats.types[0].type.name;
     
     return `
-        <div class="small-pokemon-card">
+        <div class="small-pokemon-card bg_${mainType}">
 
             <div class="small-card-top">
                 <h2>${stats.name.toUpperCase()}</h2>
