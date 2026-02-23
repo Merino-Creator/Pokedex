cabdb185ce01131209cc0fce61a409064c02bcf6
Daniel <stuermer.dan137@gmail.com>
2026-02-23 15:06:02 +0100

    finished next & prev function // added render card function // added explanations

--- scripts/modal.js
+++ scripts/modal.js
@@ -7,6 +7,7 @@ async function openDialog(index) {
     document.body.style.overflow = "hidden";
     dialogRef.showModal();
     let stats = await fetchPokeStats(index);  // reihenfolge spielt eine rolle, da wir hier erst die daten holen befor wir etwas anzeigen lassen können.
+    currentIndex = index;  // currentIndex beim öffnen auf den index setzen, damit man weis, welches index geöffnet ist und dieser auch entsprechend übergeben wird. sonst setzt er sich immer wieder zurück auf den globalen wert
     renderDialog(stats);
 }
 
@@ -58,10 +59,23 @@ async function highlightTab(tabName, id) {
     await addTabTemplate(tabName, id);
 }
 
-function showNextCard() {
+async function renderCard(index) {
+    if (!pokeStats[index]) {
+        await fetchPokeStats(index);  // überprüfen ob wir die daten geladen haben oder noch nicht
+    }
 
+    const stats = pokeStats[index];  // wir speichern die geholten daten lokal in stats um einfacher drauf zuzugreifen
+    document.getElementById('bigPokemonCard').innerHTML = dialogTemplate(stats); // wir lassen das template mit dem entsprechenden index erzeugen
+    addTabTemplate('about', stats.id);  // nachdem das template neu generiert wurde rufen wir diese funktion auf um den inhalt der tabs auch sofort zu füllen, da das nicht automatisch passiert 
+    highlightTab('about', stats.id);  // und highlight weil das auch nicht automatisch passiert. parameter wie gewohnt
+}
+
+function showNextCard() {
+    currentIndex = (currentIndex + 1) % allPokemon.length;  // wir haben 100 pokemon, also ist der maximale currentIndex 99 + 1 wäre 100 (gibts nicht) also mit % wird dividierd 100/100 = 0 und wir fangen dadurch wieder von vorne an
+    renderCard(currentIndex);
 }
 
 function showPreviousCard() {
-    
+    currentIndex = (currentIndex - 1 + allPokemon.length) % allPokemon.length;  // gleich 0 - 1 + 100 = 99, wir fangen wieder von hinten an
+    renderCard(currentIndex);
 }
