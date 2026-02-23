8f3b7e53c48c94ae27007e1ffc747cfcee85e511
Daniel <stuermer.dan137@gmail.com>
2026-02-23 13:59:42 +0100

    included functions to toggle style // created new js file // added function to img

--- script.js
+++ script.js
@@ -2,36 +2,3 @@ async function init() {
     await savePokeData();
     await addSmallPokemonCard();
 }
-
-const silverGradient = `linear-gradient(90deg,
-    #999 5%,
-    #fff 10%,
-    #ccc 30%,
-    #ddd 50%,
-    #ccc 70%,
-    #fff 80%,
-    #999 95%)`
-;
-
-const goldGradient = `linear-gradient(90deg,
-    #b8860b 5%,
-    #ffec8b 10%,
-    #daa520 30%,
-    #ffd700 50%,
-    #daa520 70%,
-    #ffec8b 80%,
-    #b8860b 95%)`
-;
-
-let isGold = false;
-
-
-function toggleMode(element) {
-    document.body.style.background = isGold ? silverGradient : goldGradient;
-    isGold = !isGold;
-
-    element.src = element.src.includes('lugia.png') 
-      ? 'assets/images/ho-oh.png' 
-      : 'assets/images/lugia.png'
-    ;
-}
---
+++ scripts/toggleStyle.js
@@ -0,0 +1,41 @@
+const silverGradient = `linear-gradient(90deg,
+    #999 5%,
+    #fff 10%,
+    #ccc 30%,
+    #ddd 50%,
+    #ccc 70%,
+    #fff 80%,
+    #999 95%)`
+;
+
+const goldGradient = `linear-gradient(90deg,
+    #b8860b 5%,
+    #ffec8b 10%,
+    #daa520 30%,
+    #ffd700 50%,
+    #daa520 70%,
+    #ffec8b 80%,
+    #b8860b 95%)`
+;
+
+let isGold = false;
+let shimmerActive = false;
+
+function toggleMode(element) {
+    document.body.style.background = isGold ? silverGradient : goldGradient;
+    isGold = !isGold;
+
+    element.src = element.src.includes('lugia.png')
+        ? 'assets/images/ho-oh.png'
+        : 'assets/images/lugia.png'
+    ;
+}
+
+function toggleShimmer() {
+    shimmerActive = !shimmerActive;
+    if (shimmerActive) {
+        document.body.classList.add('shimmer');
+    } else {
+        document.body.classList.remove('shimmer');
+    }
+}
